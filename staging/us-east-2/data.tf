8d05da2c637e82775e0d11398a91cb1e34376aee
blexy <tebidblexy36@gmail.com>
2024-09-11 20:41:02 -0400

    added initial staging infra

---
+++ staging/us-east-2/data.tf
@@ -0,0 +1,3 @@
+data "aws_route53_zone" "micablexy" {
+  name = "micablexy.com."
+}
---
+++ staging/us-east-2/variables.tf
@@ -0,0 +1,17 @@
+variable "staging_vpc_cidr" {
+  type    = string
+  default = "10.30.0.0/16"
+
+}
+
+variable "use2a" {
+  type    = string
+  default = "us-east-2a"
+
+}
+
+variable "use2b" {
+  type    = string
+  default = "us-east-2b"
+
+}
