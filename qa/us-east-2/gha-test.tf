496f7ebc2ca5d70385ea806d39e50369a7cce086
blexy <tebidblexy36@gmail.com>
2024-11-01 22:58:28 -0400

    updated synk to only report high severity in vuln

--- qa/us-east-2/gha-test.tf
+++ qa/us-east-2/gha-test.tf
@@ -6,6 +6,10 @@ resource "aws_vpc" "qa" {
   }
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = "example"
+}
+
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
 
